c3818cbbe1265bf593781e618c909d75b11acb31
luxolo <luxolo@projectcodex.co>
2016-03-31 13:06:12 +0200

    second work on best

---
+++ best_deal.js
@@ -0,0 +1,13 @@
+var avos = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+
+var items = [];
+var listAvos = avos
+.replace("for ", "for:");
+.replace("for ", "for:");
+.replace("for ", "for:");
+.replace("for ", "for:");
+console.log(listAvos);
+
+var fix = listAvos.split(" ");
+
+console.log(listAvos);
