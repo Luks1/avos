560ea6fa1834d8533923e9d0db14e5a7ab8264a6
luxolo <luxolo@projectcodex.co>
2016-04-02 17:15:39 +0200

    puting my statement on strings and array

---
+++ DOJO/Aging_Friends.js
@@ -0,0 +1,7 @@
+var myStrings = "Xola is 23, Peter is 18, Oya is 29, Ellie is 20, Sive is 27";
+
+//spliting myStrings first automatically goes to array also
+var strngs = myStrings.split(" ,");
+
+//logging out my strings will be on array
+    console.log(strngs);
