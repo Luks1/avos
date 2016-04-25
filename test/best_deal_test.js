c70af8804207162e313697cded2f96a099acfbe6
luxolo <luxolo@projectcodex.co>
2016-04-25 12:28:50 +0200

    test

--- test/best_deal_test.js
+++ test/best_deal_test.js
@@ -1,9 +1,39 @@
-// var best_deal = require('../best_deal');
-// var assert = require('assert');
-//
-// describe('A best_deal', function(){
-//   it('it should return string into an array', function(){
-//     assert.equal(best_deal(),[ ' 1 for R3', ' 2 for R7', ' 3 for R10', ' 5 for R14.50' ]
-// )
-//   })
-// });
+var string = require('../string');
+var price_per_avo = require('../price_per_avo');
+var cheapest_deal = require('../cheapest_deal');
+var expensive_deal = require('../expensive_deal');
+var average = require('../average');
+var assert = require('assert');
+
+
+var stri = " 1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
+var newStr = [ [ ' 1 ', '3' ],[ ' 2 ', '7' ],[ ' 3 ', '10' ],[ ' 5 ', '14.50' ] ]
+var prices = ["3.00", "3.50", "3.33", "2.90"]
+
+
+describe('A string', function(){
+  it('it should return string into an array', function(){
+    assert.deepEqual(string(stri),[
+  [ ' 1 ', '3' ],
+  [ ' 2 ', '7' ],
+  [ ' 3 ', '10' ],
+  [ ' 5 ', '14.50' ] ]);
+
+
+  })
+  it('it should give me the price of each avo', function(){
+
+    assert.deepEqual(price_per_avo(newStr), ["3.00", "3.50", "3.33", "2.90"]);
+  })
+  it('it shold give me the cheapest deal', function(){
+
+    assert.equal(cheapest_deal(prices),2.9)
+  })
+
+  it('it should give me the expensive deal', function(){
+    assert.equal(expensive_deal(prices), 3.5)
+  })
+it('it should give me the average', function(){
+  assert.equal(average(prices), 3.1825)
+})
+});
